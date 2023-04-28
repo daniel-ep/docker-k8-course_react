959d664e72a295201fec2aeaf64227a9730a5ddf
dan <root@dancentos.my.test.lab>
2023-04-27 20:05:43 -0600

    Changing text displayed in main Webpage

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.<br/>
-	  Bye there!
+	  I was changed in the feature branch! xD
         </p>
         <a
           className="App-link"
